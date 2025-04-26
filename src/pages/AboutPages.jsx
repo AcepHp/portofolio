e0ea1c4d57cc305fa5b9002ddb144b30e8842287
acep handika <acephandika27@gmail.com>
2025-04-26 10:00:49 +0700

    wip: slicing aboutPages

--- src/pages/AboutPages.jsx
+++ src/pages/AboutPages.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import AboutHeader from '../components/AboutHeader'
 
 export default function AboutPages() {
   return (
     <div>
         <div>
             <Navbar/>
+            <AboutHeader/>
         </div>
     </div>
   )

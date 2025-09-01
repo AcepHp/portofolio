70bf83acf7f392743d32fe485b6ef796706627aa
AcepHp <acephandika27@gmail.com>
2025-09-01 16:27:42 +0700

    fix: footerSection fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="text-gray-200 text-center md:font-medium py-6 mt-auto relative z-10">
-      <div className="mb-5 md:mb-10">
+      <div className="mb-0">
         <h1 className="text-sm md:text-md">Designed and Developed By Acep Handika.</h1>
         <h1 className="text-sm md:text-md">
           Built With <span className="text-[#3ccf91]">ReactJs</span> &{" "}

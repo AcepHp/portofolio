dbb59011791d4573b1c1ae4f868b684de667156c
AcepHp <acephandika27@gmail.com>
2025-09-01 16:25:12 +0700

    fix: footerSection fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,7 @@
-import Aos from "aos"
-import "aos/dist/aos.css"
-import { useEffect } from "react"
-
 export default function Footer() {
-  useEffect(() => {
-    Aos.init({
-      duration: 1000,
-      once: true,
-      easing: "ease-in-out",
-    })
-  }, [])
   return (
     <div className="text-gray-200 text-center md:font-medium py-6 mt-auto relative z-10">
-      <div className="mb-5 md:mb-10" data-aos="fade-down" data-aos-duration="800">
+      <div className="mb-5 md:mb-10">
         <h1 className="text-sm md:text-md">Designed and Developed By Acep Handika.</h1>
         <h1 className="text-sm md:text-md">
           Built With <span className="text-[#3ccf91]">ReactJs</span> &{" "}
